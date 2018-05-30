681e786006b55020bfb294485807b92781df931b
Emre Ates <ates@bu.edu>
2018-05-30 20:35:56 +0000

    Working on hybrid method

--- hybrid.py
+++ hybrid.py
@@ -10,20 +10,6 @@ PROJECT_ROOT = Path('~/hybrid-method').expanduser()
 CHANGESET_ROOT = Path('~/yaml/testing/').expanduser()
 
 
-def get_changeset(csid):
-    for idx, csfile in enumerate(CHANGESET_ROOT.glob('*{}*'.format(csid))):
-        if idx > 0:
-            raise IOError("Too many changesets match the csid {}".format(csid))
-        with csfile.open('r') as f:
-            changeset = yaml.load(f)
-    return changeset
-
-
-def get_scores(test_set, train_set):
-    """ Gets two lists of changeset ids """
-    print(len(test_set), len(train_set))
-
-
 def main():
     with (PROJECT_ROOT / 'changeset_sets' /
           'threek_dirty_chunks.p').open('rb') as f:
@@ -39,6 +25,51 @@ def main():
         for idx, extra_cleans in enumerate(tenks):
             train_set += extra_cleans
             get_scores(test_set, train_set)
+
+
+class Hybrid:
+    """ scikit style class for hybrid method """
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def score(self, X, y):
+        pass
+
+
+def get_changeset(csid):
+    for idx, csfile in enumerate(CHANGESET_ROOT.glob('*{}*'.format(csid))):
+        if idx > 0:
+            raise IOError("Too many changesets match the csid {}".format(csid))
+        with csfile.open('r') as f:
+            changeset = yaml.load(f)
+    return changeset
+
+
+def parse_csids(csids):
+    """ Returns labels and features from csids, features are file sets
+    file sets: list of string of format '644 /usr/.../file' """
+    features = []
+    labels = []
+    for csid in csids:
+        changeset = get_changeset(csid)
+        labels.append(changeset['label'])
+        features.append(changeset['changes'])
+    return features, labels
+
+
+def get_scores(test_set, train_set):
+    """ Gets two lists of changeset ids, does training+testing """
+    clf = Hybrid()
+    X, y = parse_csids(train_set)
+    clf.fit(X, y)
+    X, y = parse_csids(test_set)
+    clf.score(X, y)
 
 
 if __name__ == '__main__':

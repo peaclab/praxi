e594d9622d51a148a2513e36760547c6ef0ace15
Emre Ates <ates@bu.edu>
2018-05-31 15:23:19 +0000

    Working on getting vw working

--- hybrid.py
+++ hybrid.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import logging
+import logging.config
 import os
 import sys
 import pickle
@@ -8,6 +10,7 @@ import tempfile
 import yaml
 from time import sleep
 
+import envoy
 from tqdm import tqdm
 
 from columbus.columbus import columbus
@@ -17,6 +20,29 @@ CHANGESET_ROOT = Path('~/yaml/testing/').expanduser()
 
 
 def main():
+    logging.config.dictConfig({
+        'version': 1,
+        'disable_existing_loggers': True,
+        'formatters': {
+            'standard': {
+                'format': '%(asctime)s %(levelname)-7s %(message)s'
+            },
+        },
+        'handlers': {
+            'default': {
+                'level': 'DEBUG',
+                'formatter': 'standard',
+                'class': 'logging.StreamHandler',
+            },
+        },
+        'loggers': {
+            '': {
+                'handlers': ['default'],
+                'level': 'DEBUG',
+                'propagate': True
+            },
+        }
+    })
     get_scores([], [101289, 102580, 102585, 99234])
     with (PROJECT_ROOT / 'changeset_sets' /
           'threek_dirty_chunks.p').open('rb') as f:
@@ -36,22 +62,54 @@ def main():
 
 class Hybrid:
     """ scikit style class for hybrid method """
-    def __init__(self, k=15, vw_args=''):
+    def __init__(self, k=15, vw_binary='/home/ubuntu/bin/vw',
+                 vw_args='-c --loss_function hinge --redefine :=ctags -q cc '
+                         '-b 25 --passes 50 --oaa 78 -l 0.4'):
         self.k = k
+        self.vw_args = vw_args
+        self.vw_binary = vw_binary
+        self.indexed_labels = {}
+        # TODO: delete this file at __del__? after debugging
+        self.vw_modelfile = tempfile.NamedTemporaryFile('w', delete=False)
+        logging.info('Started hybrid model, vw_modelfile: %s',
+                     self.vw_modelfile.name)
 
     def fit(self, X, y):
+        logging.info('Training started')
         counter = 1
-        indexed_labels = {}
         for label in set(y):
-            indexed_labels[label] = counter
+            self.indexed_labels[label] = counter
             counter += 1
+        logging.info('Getting columbus output for %d changesets', len(X))
         tags = [columbus(x, k=self.k) for x in X]
         f = tempfile.NamedTemporaryFile('w', delete=False)
         for tag, label in zip(tags, y):
-            f.write('{} {} | {}\n'.format(
-                indexed_labels[label],
+            f.write('{} 1.0 {} | {}\n'.format(
+                self.indexed_labels[label],
                 label, ' '.join(tag)))
         f.close()
+        logging.info('vw input written to %s, starting training', f.name)
+        c = envoy.run(
+            '{vw_binary} {vw_input} {vw_args} -f {vw_modelfile}'.format(
+                vw_binary=self.vw_binary, vw_input=f.name,
+                vw_args=self.vw_args, vw_modelfile=self.vw_modelfile)
+        )
+        if c.status_code:
+            logging.error(
+                'something happened to vw, code: %d, out: %s, err: %s',
+                c.status_code, c.std_out, c.std_err)
+            raise IOError('Something happened to vw')
+        else:
+            logging.error(
+                'vw ran sucessfully. out: %s, err: %s',
+                c.std_out, c.std_err)
+
+        # # For training the model
+        # vw chosen_dirty.in -c --loss_function hinge --redefine :=ctags -q cc -b 25 --passes 50 --oaa 78 -l 0.4 -f chosen_dirty.model
+        # # For testing
+        # cd /home/ubuntu/vw/results
+        # vw -t -i ../chosen_dirty.model ../rp_ts_vw_new.in -p rp_ts_vw_new.out.11
+        # python2.7 score.py rp_ts_vw_new.out.11
 
     def predict(self, X):
         pass

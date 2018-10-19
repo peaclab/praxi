918f6461b98cf2a06df88ea90c214fe38c3debdf
Emre Ates <ates@bu.edu>
2018-10-19 14:17:19 +0000

    Set classes high initially

--- hybrid.py
+++ hybrid.py
@@ -98,8 +98,11 @@ class Hybrid(BaseEstimator):
         else:
             self.vw_args_ += ' --probabilities'
             self.loss_function = 'logistic'
-            self.vw_args_ += ' --oaa {}'.format(len(self.all_labels))
             self.vw_args_ += ' --loss_function={}'.format(self.loss_function)
+            if iterative:
+                self.vw_args_ += ' --oaa 80'
+            else:
+                self.vw_args_ += ' --oaa {}'.format(len(self.all_labels))
         if self.iterative:
             self.vw_args_ += ' --save_resume'
         tags = self._get_tags(X)

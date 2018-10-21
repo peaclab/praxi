b85a0ba7f7821c7fb5e249ded5404e3bae5567eb
Emre Ates <ates@bu.edu>
2018-10-21 20:40:10 +0000

    bug fixes

--- columbus/trie.py
+++ columbus/trie.py
@@ -1,6 +1,7 @@
-from collections import namedtuple
-
-TrieEntry = namedtuple('TrieEntry', ['count', 'child_list'])
+class TrieEntry:
+    def __init__(self):
+        self.count = 1
+        self.child_list = []
 
 class Trie:
     def __init__(self, frequency_limit=2):
@@ -12,11 +13,11 @@ class Trie:
             return
         token = token.replace(':', '').replace('|', '')
         parent = None
-        while token:
+        while len(token) >= 3:
             if token in self.map:
                 self.map[token].count += 1
             else:
-                self.map[token] = TrieEntry(count=1, child_list=[])
+                self.map[token] = TrieEntry()
             if parent and parent not in self.map[token].child_list:
                 self.map[token].child_list.append(parent)
             parent = token
@@ -26,5 +27,5 @@ class Trie:
         results = {}
         for token, entry in self.map.items():
             if len(entry.child_list) >= self.frequency_limit:
-                self.results[token] = entry.count
+                results[token] = entry.count
         return results
